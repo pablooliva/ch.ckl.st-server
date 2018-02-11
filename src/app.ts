b805f28765db5c7a25fd64174860194ab65ca191
Pablo Oliva <pablo@qecept.com>
2018-02-11 02:37:10 +0100

    TS seed with testing set up.

--- src/app.ts
+++ src/app.ts
@@ -7,38 +7,46 @@ import * as cookieParser from "cookie-parser";
 import * as bodyParser from "body-parser";
 import * as dotenv from "dotenv";
 
-// Load environment variables from .env file, where API keys and passwords are configured
-dotenv.config({ path: ".env" });
+class App {
+    public express: express.Application;
 
-const appRoutes = require("./routes/app.routes");
+    public constructor() {
+        this.express = express();
+        this.config();
+    }
 
-const app = express();
+    private config(): void {
+        const appRoutes = require("./routes/app.routes");
 
-app.set("views", path.join(__dirname, "public"));
-app.set("view engine", "html");
-app.engine("html", function(path: any, options: any, cb: any) {
-    fs.readFile(path, "utf-8", cb);
-});
+        dotenv.config({ path: ".env" });
 
-app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+        this.express.set("views", path.join(__dirname, "public"));
+        this.express.set("view engine", "html");
+        this.express.engine("html", function(path: any, options: any, cb: any) {
+            fs.readFile(path, "utf-8", cb);
+        });
 
-app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, OPTIONS");
-    next();
-});
+        // this.express.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+        this.express.use(logger("dev"));
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({ extended: false }));
+        this.express.use(cookieParser());
+        this.express.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", appRoutes);
+        this.express.use(function (req, res, next) {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, OPTIONS");
+            next();
+        });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    return res.render("index");
-});
+        this.express.use("/", appRoutes);
 
-module.exports = app;
+        // catch 404 and forward to error handler
+        this.express.use(function(req, res, next) {
+            return res.render("index");
+        });
+    }
+}
+
+export default new App().express;
