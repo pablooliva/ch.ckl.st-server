fd91488165dd28a78299999f9df23b69dd8453a3
Pablo Oliva <pablo@qecept.com>
2018-12-05 18:06:05 +0100

    Improvements to running Docker Container remotely.

--- build.utils.js
+++ build.utils.js
@@ -1,13 +1,10 @@
 const shell = require("shelljs");
 
-if (process.env.NODE_ENV === "rmt-dkr-prod") {
-    shell.rm("-Rf", "dist/");
-    shell.mkdir("dist");
-    shell.cp("-R", "src/public/", "dist/public/");
-} else {
-    shell.rm("-Rf", "dist/");
-    shell.mkdir("dist");
-    shell.cp("-R", "src/public/", "dist/public/");
+shell.rm("-Rf", "dist/");
+shell.mkdir("dist");
+shell.cp("-R", "src/public/", "dist/public/");
+
+if (process.env.SERVER === "local") {
     shell.mkdir("dist/config");
     shell.cp("-f", "src/config/.env", "dist/config/.env");
 }
