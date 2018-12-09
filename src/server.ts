f11a1ac481476233890ae98cc4ba1f819fde7ca5
Pablo Oliva <pablo@qecept.com>
2018-12-09 03:10:32 +0100

    Prep for production enviroment.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-// import * as https from "https";
-// import * as fs from "fs";
+import * as https from "https";
+import * as fs from "fs";
 
 import { App } from "./config/express";
 import { ClstLogger } from "./config/logger";
@@ -7,21 +7,26 @@ import { ClstLogger } from "./config/logger";
 const log = ClstLogger.log();
 const app = new App().express;
 const port = process.env.PORT || 3000;
-const server = app;
+const sslPort = process.env.SSL_PORT || 443;
+let server;
+let listeningPort;
 
-/*if (process.env.NODE_ENV === "dev") {
+if (process.env.NODE_ENV === "prod" && process.env.SERVER !== "local") {
   const serverOptions = {
-    key: fs.readFileSync("src/config/certs/rootCA.key"),
-    cert: fs.readFileSync("src/config/certs/rootCA.crt")
+    key: fs.readFileSync("/etc/letsencrypt/live/api.ch.ckl.st/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/api.ch.ckl.st/fullchain.pem"),
+    ca: fs.readFileSync("/etc/letsencrypt/live/api.ch.ckl.st/chain1.pem")
   };
   server = <any>https.createServer(serverOptions, app);
+  listeningPort = sslPort;
 } else {
   server = app;
-}*/
+  listeningPort = port;
+}
 
-server.listen(port, (err: Error) => {
+server.listen(listeningPort, (err: Error) => {
   if (err) {
     return log.error(" *** Server error *** " + err.toString());
   }
-  log.info(`Express server listening on port ${port}. Environment: ${process.env.NODE_ENV}.`);
+  log.info(`Express server listening on port ${listeningPort}. Environment: ${process.env.NODE_ENV}.`);
 });
