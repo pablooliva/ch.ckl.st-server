a91d63defcaee7840469187777fa0e9c68fcf255
Pablo Oliva <p.o@pablooliva.de>
2020-06-01 20:31:11 +0200

    Directory adjustments for the new DCL server.

--- update-dcl-server.sh
+++ update-dcl-server.sh
@@ -2,4 +2,4 @@
 # to be run in Droplet
 docker pull pablooliva/ch.ckl.st-server
 docker stack rm chcklst-server
-docker stack deploy -c /root/chcklst/server/docker-compose-srvr-prod.yml chcklst-server
+docker stack deploy -c /home/chcklst/server/docker-compose-srvr-prod.yml chcklst-server
